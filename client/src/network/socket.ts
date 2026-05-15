1e41f7dc2a0c70b4f933e06573233eeecbbb8036
Niraj <port.iamniraj@gmail.com>
2026-05-15 23:16:22 +0530

    bug: port

--- client/src/network/socket.ts
+++ client/src/network/socket.ts
@@ -1,3 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000");
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+
+export const socket = io(SERVER_URL);
